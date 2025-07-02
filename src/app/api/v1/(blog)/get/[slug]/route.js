d00953d6531cc5748d68f5e9ed27971dcc67081a
Harshal-Khairnar01 <harshalkhairnar7244@gmail.com>
2025-07-02 15:41:51 +0530

    admin all users page done

--- src/app/api/v1/(blog)/get/[slug]/route.js
+++ src/app/api/v1/(blog)/get/[slug]/route.js
@@ -13,6 +13,7 @@ export async function GET(request, { params }) {
         select: {
           name: true,
           image: true,
+          username:true,
         },
       },
     },
--- src/app/blog/[slug]/page.jsx
+++ src/app/blog/[slug]/page.jsx
@@ -3,6 +3,7 @@ import { Calendar } from "lucide-react";
 import Image from "next/image";
 import "@/styles/blog.css";
 import { notFound } from "next/navigation";
+import Link from "next/link";
 
 const fetchSingleBlog = async (slug) => {
   const res = await fetch(
@@ -11,7 +12,7 @@ const fetchSingleBlog = async (slug) => {
       next: { tags: [slug] },
     }
   );
-  if(res.status==404){
+  if (res.status == 404) {
     notFound();
   }
   const data = await res.json();
@@ -47,11 +48,23 @@ export default async function SigleBlog({ params }) {
         )}
         <h1 className=" text-2xl md:text-4xl font-bold">{post.title}</h1>
         <div className=" meta-of-a-blog space-y-2">
-          <div className=" flex gap-2  items-center">
+          <div className="  flex flex-col sm:flex-row gap-5">
+            <div className=" flex gap-2  items-center">
             <Calendar className=" text-gray-400 size-4" />
             <p className=" text-gray-400 text-xs">
               Created on: {dateFormat(post.createdAt)}
             </p>
+          </div>
+          <Link className=" flex gap-2 items-center justify-center" href={`/user/${post.author.username}`}>
+            <Image
+            className=" rounded-full"
+              src={post.author.image}
+              width={20}
+              height={20}
+              alt={post.author.name}
+            />
+            <p className=" text-xs text-gray-400">{post.author.name}</p>
+          </Link>
           </div>
           <div className=" text-xs flex items-center gap-2">
             <p>Categories:</p>
@@ -64,7 +77,7 @@ export default async function SigleBlog({ params }) {
               <p>Tags:</p>
               {post?.keywords.split(",").map((tag, index) => (
                 <p
-                  key={tag+index}
+                  key={tag + index}
                   className="  badge bg-gray-600/40 border  border-gray-700 w-fit px-2 py-1 rounded"
                 >
                   {tag}
@@ -75,7 +88,7 @@ export default async function SigleBlog({ params }) {
         </div>
         <div
           className=" blogContent text-sm w-[90%] md:w-2/3 text-gray-300"
-          dangerouslySetInnerHTML={{ __html: post.Content||"" }}
+          dangerouslySetInnerHTML={{ __html: post.Content || "" }}
         ></div>
       </div>
     </section>

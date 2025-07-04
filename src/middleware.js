6c71029634c8f019f5639ca8ea91f35686375ba9
Harshal-Khairnar01 <harshalkhairnar7244@gmail.com>
2025-07-04 18:04:54 +0530

    middleware added for cors

---
+++ src/middleware.js
@@ -0,0 +1,33 @@
+import { NextResponse } from 'next/server';
+
+const allowedOrigins = ['http://localhost:3000', 'https://typenest.vercel.app'];
+
+export async function middleware(request) {
+  const origin = request.headers.get('origin');
+
+ 
+  if (request.method === 'OPTIONS') {
+    if (origin && allowedOrigins.includes(origin)) {
+      const response = NextResponse.next();
+      response.headers.set('Access-Control-Allow-Origin', origin);
+      response.headers.set('Access-Control-Allow-Methods', 'GET,POST,OPTIONS');
+      response.headers.set('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+      return response;
+    } else {
+      return NextResponse.json({ message: 'CORS Blocked || Unauthorized' }, { status: 403 });
+    }
+  }
+
+
+  if (request.method === 'POST') {
+    if (origin && !allowedOrigins.includes(origin)) {
+      return NextResponse.json({ message: 'CORS Blocked || Unauthorized' }, { status: 403 });
+    }
+  }
+
+  return NextResponse.next();
+}
+
+export const config = {
+  matcher: '/api/v1/:path*',
+};
